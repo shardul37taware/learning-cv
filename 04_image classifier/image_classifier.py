8ab372ef3719746d254c357546e5bde6d321ae95
shardul37taware <shardul.santosh.taware@gmail.com>
2025-05-06 19:00:35 +0530

    Update image_classifier.py

--- 04_image classifier/image_classifier.py
+++ 04_image classifier/image_classifier.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
+import pickle
 
 # prepare data
 input_dir = 'D:/sst/disaster/dataset raw'
@@ -26,11 +27,11 @@ for category_idx, category in enumerate(categories):
 data = np.asarray(data)
 labels = np.asarray(labels)
 
-
+print(1)
 # test train split
 x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
-
+print(2)
 # train classifier
 classifier = SVC()
 parameters = [{'gamma':[0.01, 0.001, 0.0001], 'C':[1, 10, 100, 1000]}]
@@ -38,10 +39,12 @@ parameters = [{'gamma':[0.01, 0.001, 0.0001], 'C':[1, 10, 100, 1000]}]
 grid_search = GridSearchCV(classifier, parameters)
 grid_search.fit(x_train, y_train)
 
-
+print(3)
 # test performance
 best_estimator = grid_search.best_estimator_
 y_prediction = best_estimator.predict(y_test)
 score = accuracy_score(y_prediction, y_test)
 
 print('{}% of samples were correctly classified'.format(str(score*100)))
+
+pickle.dump(best_estimator, open('./model.p', 'wb'))
