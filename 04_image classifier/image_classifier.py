0d9c5a16edfc1c7b3368ea46526deaeed9b367e0
shardul37taware <shardul.santosh.taware@gmail.com>
2025-05-07 18:51:48 +0530

    image loader improvement

--- 04_image classifier/image_classifier.py
+++ 04_image classifier/image_classifier.py
@@ -10,28 +10,40 @@ from sklearn.metrics import accuracy_score
 
 import pickle
 
+from PIL import Image
+
+print('0')
 # prepare data
-input_dir = 'D:/sst/disaster/dataset raw'
+input_dir = 'D:/sst/disaster/dataset/result/test'
 categories = ['damage','fire','flood','normal']
 
 data = []
 labels = [] 
+valid_extensions = ['.jpg', '.jpeg', '.png', '.bmp', '.tiff']
+
 for category_idx, category in enumerate(categories):
-    for file in os.listdir(os.path.join(input_dir, category)):
-        img_path = os.path.join(input_dir, category, file)
-        img = imread(img_path)
-        img = resize(img, (15, 15))
-        data.append(img.flatten())
-        labels.append(category_idx) 
+    category_path = os.path.join(input_dir, category)
+    for file in os.listdir(category_path):
+        if not any(file.lower().endswith(ext) for ext in valid_extensions):
+            continue
+        img_path = os.path.join(category_path, file)
+        print(f"Processing: {img_path}")
+        try:
+            img = Image.open(img_path).convert('RGB')
+            img = img.resize((15, 15))
+            data.append(np.asarray(img).flatten())
+            labels.append(category_idx)
+        except Exception as e:
+            print(f"Error with file {img_path}: {e}") 
 
 data = np.asarray(data)
 labels = np.asarray(labels)
 
-print(1)
+print('1')
 # test train split
 x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
-print(2)
+print('2')
 # train classifier
 classifier = SVC()
 parameters = [{'gamma':[0.01, 0.001, 0.0001], 'C':[1, 10, 100, 1000]}]
@@ -39,7 +51,7 @@ parameters = [{'gamma':[0.01, 0.001, 0.0001], 'C':[1, 10, 100, 1000]}]
 grid_search = GridSearchCV(classifier, parameters)
 grid_search.fit(x_train, y_train)
 
-print(3)
+print('3')
 # test performance
 best_estimator = grid_search.best_estimator_
 y_prediction = best_estimator.predict(y_test)
